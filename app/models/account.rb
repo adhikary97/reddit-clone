f8b1c3f698853f6d01439f3abfd5ae33863d6dd2
adhikary97 <adhikary@purdue.edu>
2020-05-17 03:03:05 -0500

    added comments to posts, and subscriptions

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,4 +5,12 @@ class Account < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :communities
   has_many :posts
+  has_many :comments
+
+  validates_presence_of :first_name, :last_name, :username
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
